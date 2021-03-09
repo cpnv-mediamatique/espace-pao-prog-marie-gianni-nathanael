dc4613b42fce98ac9ba69ab8c061fe042014da1c
Gianni Gritti <Gianni.Gritti@cpnv.ch>
2021-03-09 16:24:24 +0100

    correction

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=espaceentreptise;charset=utf8", "root", "");
+    $bdd = new PDO("mysql:host=localhost;dbname=espaceentreprise;charset=utf8", "root", "");
 } catch (Exception $e) {
     die("Erreur : " . $e->getMessage());
 }
