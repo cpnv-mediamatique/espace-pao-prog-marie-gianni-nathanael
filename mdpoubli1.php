edb8438049132ea2ddd8dc0657a97a565603b557
Gianni Gritti <Gianni.Gritti@cpnv.ch>
2021-03-09 16:12:33 +0100

    message

---
+++ mdpoubli1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mot de passe oublié ?</title>
+</head>
+<body>
+    <p>Votre mail :</p>
+    <form action="mdpoubli.php" >
+        <input type="email">
+        <input type="submit">
+    </form>
+</body>
+</html>
