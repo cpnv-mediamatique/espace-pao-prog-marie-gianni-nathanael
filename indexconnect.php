4ce3f548415992cb23dd28bcda6a2f2a418fb404
Marie Bovay <marie.bovay@hotmail.ch>
2021-03-09 13:52:37 +0100

    Changement extension des fichiers

---
+++ indexconnect.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/favicon.png" type="image/x-icon" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <!-- Notre CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+
+    <!-- La NavBar -->
+    <div class="container-max-widths">
+        <nav class="navbar navbar-dark primary-color retourSansDeco py-0">
+
+            <div class="m-4">
+                <img class="mx-4" src="img/Espace_entreprise_logo.svg" style="width: 8rem" />
+            </div>
+            <div class="row">
+                <div class="mx-4">
+                    <a href="statut.html">Statuts</a>
+                    <a class="mx-4" href="commandes.html">Commandes</a>
+                    <a href="index.html">
+                        <button type="button" class="btn btn-success">Se déconnecter</button>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- Impressions -->
+    <div class="backgroundColorFonce">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-3 mt-5 order-2 order-lg-1">
+                    <div class="card">
+                        <div class="fondVert shadow-1-strong">
+                            <div class="card-top mt-5 mb-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                    class="bi bi-printer fondVert" viewBox="0 0 16 16">
+                                    <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                                    <path
+                                        d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body mt-1">
+                            <div class="centrerCartes">
+                                <h5 class="card-title">Impressions</h5>
+                                <h7 class="card-title">Format A4/A3</h1>
+                                    <hr size="4px">
+                                    <p class="card-text">
+                                        Nous nous chargeons de vos impressions avec choix du grammage.
+                                    </p>
+                                    <a href="#!" class="btn btn-success">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Texte Bienvenue -->
+                <div class="col-lg-5 d-flex justify-content-center order-1 order-lg-2 pasDeRepet">
+                    <div class="d-flex justify-content-center">
+                        <div class="arrow-down">
+                            <div class="texteDeBienvenueTitre mt-5">
+                                Bienvenue dans l'espace entreprise-PAO !
+                                <div class="texteDeBienvenue mt-2">
+                                    Vous trouvez ci-dessous les services couverts par nos soins. Nous espérons que nos
+                                    produits vous
+                                    seront
+                                    utiles !
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Carte de visites -->
+                <div class="col-lg-3 mt-5 order-3 order-lg-3">
+                    <div class="card">
+                        <div class="fondVert shadow-1-strong">
+                            <div class="card-top mt-5 mb-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                    class="bi bi-card-text fondVert" viewBox="0 0 16 16">
+                                    <path
+                                        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                                    <path
+                                        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body mt-1">
+                            <div class="centrerCartes">
+                                <h5 class="card-title">Cartes de visite</h5>
+                                <h7 class="card-title">Gratuit</h1>
+                                    <hr size="4px">
+                                    <p class="card-text">
+                                        Des cartes de visite sur mesure, avec des designs et une impression de qualité.
+                                    </p>
+                                    <a href="cartesVisite.html" class="btn btn-success">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Les étiquettes -->
+            <div class="row">
+                <div class="col-lg-4 p-lg-5">
+                    <div class="card m-3">
+                        <div class="fondVert shadow-1-strong">
+                            <div class="card-top mt-5 mb-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                    class="bi bi-tag fondVert" viewBox="0 0 16 16">
+                                    <path
+                                        d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                    <path
+                                        d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586l7 7L13.586 9l-7-7H2v4.586z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body mt-1">
+                            <div class="centrerCartes">
+                                <h5 class="card-title">Les étiquettes</h5>
+                                <h7 class="card-title">Taille 3.4 x 6.6 cm</h1>
+                                    <hr size="4px">
+                                    <p class="card-text">
+                                        Étiquettes collantes pouvant servir pour les armoires de classe, les casiers
+                                        téléphones ou d’autres
+                                        usages.
+                                    </p>
+                                    <a href="etiquettes.html" class="btn btn-success">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Répartitions des tâches -->
+                <div class="col-lg-4 p-lg-5">
+                    <div class="card m-3">
+                        <div class="fondVert shadow-1-strong">
+                            <div class="card-top mt-5 mb-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                    class="bi bi-calendar2-check fondVert" viewBox="0 0 16 16">
+                                    <path
+                                        d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                                    <path
+                                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                                    <path
+                                        d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body mt-1">
+                            <div class="centrerCartes">
+                                <h5 class="card-title">Répartitions des tâches</h5>
+                                <h7 class="card-title">Planning de nettoyage</h1>
+                                    <hr size="4px">
+                                    <p class="card-text">
+                                        Je comprends pas s'ils font que les plannings de nettoyage ou pleins d'autres
+                                        tableaux.
+                                    </p>
+                                    <a href="#!" class="btn btn-success">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Casiers pour téléphones -->
+                <div class="col-lg-4 p-lg-5">
+                    <div class="card m-3">
+                        <div class="fondVert shadow-1-strong">
+                            <div class="card-top mt-5 mb-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                    class="bi bi-phone fondVert" viewBox="0 0 16 16">
+                                    <path
+                                        d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body mt-1">
+                            <div class="centrerCartes">
+                                <h5 class="card-title">Casiers pour téléphones</h5>
+                                <h7 class="card-title">Casiers en cartons ou en mousse</h1>
+                                    <hr size="4px">
+                                    <p class="card-text">
+                                        Pour que les élèves gardent une concentration maximale pendant les cours.
+                                    </p>
+                                    <a href="#!" class="btn btn-success">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+<!-- MDB -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<!-- Custom scripts -->
+<script type="text/javascript"></script>
+
+</html>
